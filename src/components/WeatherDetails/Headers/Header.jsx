7447466e154bd64e6597ee423a2ac8ae706a5402
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2023-03-16 12:00:09 +0100

    onclick functionality added

--- src/components/WeatherDetails/Headers/Header.jsx
+++ src/components/WeatherDetails/Headers/Header.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 
-function Header({units, setUnits}) {
+function Header({units, setUnits, setOptions}) {
+  const [clickedUnit, setClickedUnit] = useState("metric")
+  const [option, setOption] = useState("hourly")
 
   const handleUnitChange = (e) => {
     const unit = e.currentTarget.name;
+    setClickedUnit(unit)
     if(units !== unit){
       setUnits(unit)
     }
   }
 
+  const handlehourlyOrWeeklyChange = (e) => {
+    const hourlyOrWeekly = e.currentTarget.name;
+    setOption(hourlyOrWeekly)
+    setOptions(hourlyOrWeekly)
+  }
+
   return (
     <div className="header">
         <div className="dailyOrWeekly">
-            <p>Today</p>
-            <p>Week</p>
+            <button
+             name="hourly"
+             onClick={handlehourlyOrWeeklyChange}
+             className={option === "hourly" ? "option__select" : ""}
+            >
+              Today
+            </button>
+            <button
+              name="weekly"
+              onClick={handlehourlyOrWeeklyChange}
+              className={option === "weekly" ? "option__select" : ""}
+            >
+              Week
+            </button>
         </div>
 
         <div className="degree">
             <button
               name="metric"
               onClick={handleUnitChange}
+              className={clickedUnit === "metric" ? "unit__select" : ""}
             >°C
             </button>
             <button
               name="imperial"
               onClick={handleUnitChange}
+              className={clickedUnit === "imperial" ? "unit__select" : ""}
               >°F
             </button>
         </div>
