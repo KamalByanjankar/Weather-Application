96384d0719112e6f1b222b35809a305f7d3d7a24
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2023-03-30 14:10:41 +0200

    weather gif added accoriding to the weather description

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -5,11 +5,28 @@ import Temperature from './Temperature/Temperature'
 import './WeatherForecast.css'
 
 function WeatherForecast({weather, setQuery, units}) {
+  const description = weather.description;
+  var weatherName;
+  switch(description){
+    case ("clear sky"):
+      weatherName = "weatherforecast sunnyday"
+      break;
+    case ("few clouds"):
+    case ("broken clouds"):
+    case ("scattered clouds"):
+      weatherName = "weatherforecast dayfewclouds"
+      break;
+    case ("light rain"):
+      weatherName = "weatherforecast dayrainfall"
+      break;
+    default:
+      weatherName = "weatherforecast"
+  }
   return (
-    <div className="weatherforecast">
-        <Inputs setQuery={setQuery}/>
-        <Temperature weather={weather} units={units} />
-        <Details weather={weather} />
+    <div className={weatherName}>
+      <Inputs setQuery={setQuery}/>
+      <Temperature weather={weather} units={units} />
+      <Details weather={weather} />
     </div>
   )
 }
